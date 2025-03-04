f908e8522a5a56af403807d0914b021ce527bc67
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-04 11:58:52 +0100

    feat: Add Footer component for enhanced site branding and navigation

---
+++ docs/.vitepress/Footer.vue
@@ -0,0 +1,40 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #layout-bottom>
+      <footer class="footer">
+        <div class="footer-content">
+          <p>
+            © {{ new Date().getFullYear() }} Stunk. Built with ❤️ by AbdulAzeez
+            Olanrewaju
+          </p>
+          <p>
+            <a href="https://github.com/I-am-abdulazeez/stunk" target="_blank"
+              >GitHub</a
+            >|
+            <a href="https://x.com/dev_azeez" target="_blank">Twitter</a>
+          </p>
+        </div>
+      </footer>
+    </template>
+  </DefaultTheme.Layout>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 20px;
+  font-size: 14px;
+  background-color: #030712;
+  color: #f3f3f3;
+  border-top: 1px solid #ddd;
+}
+.footer a {
+  color: #007bff;
+  text-decoration: none;
+  margin: 0 10px;
+}
+</style>
