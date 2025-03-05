adc5f1efab840036d36cfaabc85fcd14c39b8a12
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-05 16:21:49 +0100

    feat: Add StunkLogo component with responsive styling

---
+++ docs/.vitepress/StunkLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="stunk-logo">
+    <img src="/logo.svg" alt="Stunk Logo" class="logo" />
+  </div>
+</template>
+
+<style scoped>
+.stunk-logo {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.logo {
+  height: 24px;
+  width: 24px;
+}
+</style>
