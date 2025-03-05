e3ad621b33bfb2b14842f4adeb704cb0bbfd095a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-05 16:22:44 +0100

    feat: Integrate StunkLogo component and conditionally render footer in layout

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,21 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Footer from '../Footer.vue'
+import StunkLogo from '../StunkLogo.vue'
+
+import { useRoute } from 'vitepress'
 
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const route = useRoute()
+
     return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(Footer)
+      'layout-bottom': () => (route.path === '/' ? h(Footer) : null),
+      'nav-bar-title-before': () => h(StunkLogo)
     })
   },
   enhanceApp({ app, router, siteData }) {
