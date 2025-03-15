bca89a30f69be18181008995c268b817263b1234
dreezy305 <bankoleidris@gmail.com>
2025-03-15 22:47:58 +0100

    fix: dependecy issues with shiki

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -35,7 +35,7 @@ import {
   unref,
   watch,
   watchEffect
-} from "./chunk-LW4I4DCF.js";
+} from "./chunk-VJWGEPT5.js";
 
 // node_modules/@vueuse/shared/index.mjs
 function computedEager(fn, options) {
